e35674ab0c15074833a02c432ef2d13335d2580c
Marat200118 <marat.samigullin1@gmail.com>
2023-11-09 16:24:27 +0100

    third task completed

--- exercise_03_start/js/script.js
+++ exercise_03_start/js/script.js
@@ -1,13 +1,41 @@
 let count = 0;
 const maxPlays = 3;
 let tl;
-const init = () => {
 
+const init = () => {
   console.log(gsap.version);
   gsap.set(".banner", { autoAlpha: 1 });
+  tl = gsap.timeline({ repeat: maxPlays });
 
+  tl.from("h1", {
+    duration: 1,
+    opacity: 0,
+    scale: 0,
+    stagger: 0.001,
+    ease: "power3.out",
+  });
 
+  tl.from(".item", {
+    duration: 1,
+    opacity: 0,
+    scale: 0,
+    stagger: 0.2,
+    ease: "power3.out",
+  });
 
-}
+  tl.from(".footer", {
+    duration: 1,
+    opacity: 0,
+    width: 0,
+    ease: "power3.out",
+    stagger: 0.2,
+  });
+
+  if (count === 3) {
+    tl.from(".footer", {
+      backgroundColor: "green",
+    });
+  }
+};
 
 init();
