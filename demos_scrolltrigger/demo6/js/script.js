e94ce29d4a752858c08e4baa52fb7b07f09b5d18
Marat200118 <marat.samigullin1@gmail.com>
2023-11-16 15:37:27 +0100

    Scroll trigger demos added

---
+++ demos_scrolltrigger/demo6/js/script.js
@@ -0,0 +1,18 @@
+const init = () => {
+  //
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.to(".square", {
+    x: 700,
+    duration: 3,
+    scrollTrigger: {
+      trigger: ".square",
+      start: "top 70%",
+      end: "bottom 20%",
+      toggleClass: "white",
+      markers: { fontSize: "25px", fontWeight: "bold" },
+      scrub: 0.5, //true
+    },
+  });
+};
+init();
